7d8c056339bb2345cd6f7ba1fc290945873c8781
Ysrael Moreno <ysrael.moreno@ab-inbev.com>
2025-05-27 11:15:31 -0300

    create blog repository

--- src/modules/blog/repository/blog.repository.ts
+++ src/modules/blog/repository/blog.repository.ts
@@ -9,7 +9,32 @@ export class BlogRepository {
       description: 'Lorem Ipsum',
       applicant: 'Test User',
       bloodType: 'O+',
-      image: 'image',
+      image:
+        'https://images.unsplash.com/photo-1615461066159-fea0960485d5?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Ymxvb2QlMjBkb25hdGlvbnxlbnwwfHwwfHx8MA%3D%3D',
+    },
+    {
+      title: 'Title Teste',
+      description: 'Lorem Ipsum',
+      applicant: 'Test User',
+      bloodType: 'O+',
+      image:
+        'https://images.unsplash.com/photo-1615461066159-fea0960485d5?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Ymxvb2QlMjBkb25hdGlvbnxlbnwwfHwwfHx8MA%3D%3D',
+    },
+    {
+      title: 'Title Teste',
+      description: 'Lorem Ipsum',
+      applicant: 'Test User',
+      bloodType: 'O+',
+      image:
+        'https://images.unsplash.com/photo-1615461066159-fea0960485d5?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Ymxvb2QlMjBkb25hdGlvbnxlbnwwfHwwfHx8MA%3D%3D',
+    },
+    {
+      title: 'Title Teste',
+      description: 'Lorem Ipsum',
+      applicant: 'Test User',
+      bloodType: 'O+',
+      image:
+        'https://images.unsplash.com/photo-1615461066159-fea0960485d5?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Ymxvb2QlMjBkb25hdGlvbnxlbnwwfHwwfHx8MA%3D%3D',
     },
   ];
 
